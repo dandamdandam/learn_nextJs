c1e6c1a8f6275c47389c2e70dae0bfd77f5c4229
dandamdandam <glue0440@gmail.com>
2024-03-15 17:20:58 +0900

    [Feat] - write style

--- app/(write)/write/page.tsx
+++ app/(write)/write/page.tsx
@@ -5,6 +5,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 
 import Header from "../../components/header";
 import { backURL } from "../../globalVariable";
+import style from "../../styles/write.module.css";
 
 // https://velog.io/@sbinha/next.js-Link%EB%A1%9C-props-%EC%A0%84%EB%8B%AC%ED%95%98%EA%B8%B0
 const WritePage = () => {
@@ -32,32 +33,38 @@ const WritePage = () => {
     return(
         <div>
             <Header />
-            <form method="post" onSubmit={onSubmit}>
-                <div>
-                    <label htmlFor="exerciseName">오늘 한 운동: </label>
-                    <input
-                        id="exerciseName"
-                        name="exerciseName"
-                        defaultValue={params.get('exerciseName')}
-                        type="text"
-                    />
-
-                    <label htmlFor="duration">한 시간: </label>
-                    <input
-                        id="duration"
-                        name="duration"
-                        defaultValue={params.get('duration')}
-                        type="time"
-                    />
-
-                    <label htmlFor="content">메모: </label>
-                    <textarea
-                        id="content"
-                        name="content"
-                        defaultValue={params.get('content')}
-                    />
+            <form method="post" onSubmit={onSubmit} className={style.form}>
+                <div className={style.inputs}>
+                    <div>
+                        <label htmlFor="exerciseName">오늘 한 운동: </label>
+                        <input
+                            id="exerciseName"
+                            name="exerciseName"
+                            defaultValue={params.get('exerciseName')}
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="duration">한 시간: </label>
+                        <input
+                            id="duration"
+                            name="duration"
+                            defaultValue={params.get('duration')}
+                            type="time"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="content">메모: </label>
+                        <textarea
+                            id="content"
+                            name="content"
+                            defaultValue={params.get('content')}
+                        />
+                    </div>
                 </div>
-                <button type="submit">&gt; 저장하기 &lt;</button>
+                <div className={style.button}>
+                    <button className="colored_span" type="submit">&gt; 저장하기 &lt;</button>
+                </div>
             </form>
         </div>
     );
